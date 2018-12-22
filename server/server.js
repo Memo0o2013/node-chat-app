3e3947fbac4752b85426d0fecb1c6ee5838e328e
Memo0o2013 <mohamedabdelaal2013@gmail.com>
2018-12-22 21:24:11 +0200

    add Geolocation support

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage , generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname , '../public');
 const app = express();
@@ -25,6 +25,10 @@ io.on('connection' , (socket) => {
     socket.on('createMessage' , (message) => {
         console.log('MessageCreated' , message);
         io.emit('newMessage' ,generateMessage(message.from , message.text));
+    });
+
+    socket.on('createMessageLocation' , (coords) => {
+        io.emit('newLocationMessage' , generateLocationMessage('Admin!' , coords.latitude, coords.longitude));
     });
 });
 
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message');
+const {generateMessage , generateLocationMessage} = require('./message');
 
 describe('generateMessage' , () => {
     it('Should have correct message' , () => {
@@ -9,5 +9,18 @@ describe('generateMessage' , () => {
 
         expect(message.createdAt).toBeGreaterThan(1);
         expect(message).toMatchObject({from:'Memo',text:"My Message"});
+    });
+});
+
+describe ('generateLocationMessage' , () => {
+    it('Should be a valid url ' , () => {
+        let from = 'Rob';
+        let latitude = 15;
+        let longitude = 20;
+        let url = 'https://www.google.com/maps?q=15,20';
+        let message = generateLocationMessage(from , latitude ,longitude);
+
+        expect(message.createdAt).toBeGreaterThan(1);
+        expect(message).toMatchObject({from:'Rob',url:"https://www.google.com/maps?q=15,20"});
     })
 })
